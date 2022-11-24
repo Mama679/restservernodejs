b1b94900a2bba34390a7caa7dd061836e96ca7a5
Mario Manotas <mrmanotas@personalsoft.com.co>
2022-11-24 13:01:30 -0500

    Configuración BD REmota

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async()=>{
     try{
-            await mongoose.connect(process.env.MONGO_CNN2);
+            await mongoose.connect(process.env.MONGO_CNN);
             console.log('Base de Datos Online');
     }catch(error){
         console.log(error);
